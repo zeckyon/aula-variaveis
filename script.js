72d026406566d79927cfc6751a91a0c625dae137
zeckyon <129232416+zeckyon@users.noreply.github.com>
2023-04-06 21:47:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Exercício 1 escrita de código
+let nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+
+alert("Olá " + nome + ", você tem " + idade + " anos de idade.")*/
+
+// Exercício 2 escrita de código
+
+/*let respostaUm = prompt("Você quer se tornar programador? (Sim/Não)")
+let respostaDois = prompt ("Você faz faculdade? (Sim/Não)")
+let respostaTres = prompt ("O Internacional é pequeno? (Sim/Não)")
+
+alert("Você quer se tornar programador? " + respostaUm + "." + "\nVocê faz faculdade? " + respostaDois + ".\nO Internacional é pequeno? " + respostaTres + ".Sim") */
+
+// Exercício 3 escrita de código
+
+/*let a = 10
+let b = 25
+let c = a
+a = b
+b = c
+
+alert("O novo valor de a é " + a + "." + "\nO novo valor de b é " + b + ".")*/
+
+// DESAFIO
+
+let valor1 = parseFloat(prompt("Digite um valor:"))
+let valor2 = parseFloat(prompt("Digite um segundo valor:"))
+
+soma = valor1 + valor2
+multiplicacao = valor1 * valor2
+
+alert("O primeiro número somado ao segundo número resulta em: " + soma + "." + "\nO primeiro número multiplicado pelo segundo número resulta em: " + multiplicacao + ".")
